750759546b97e95bd7bcd0ccf943de35e41e6779
joaoportolan93 <162385602+joaoportolan93@users.noreply.github.com>
2024-04-25 22:44:45 +0000

    a

--- for3.cpp
+++ for3.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
  using namespace std;
- int main(){}
+ int main(){
+    int num_linhas;
+    cout << "Quantidade de linhas: "; 
+    cin >> num_linhas;
+    for (int i = 1; i <= num_linhas; i++){
+        for(int j = 0; j < i; j++)(
+            cout << "*"
+        );
+        cout << endl;
+    }
+    return 0;
+ }
